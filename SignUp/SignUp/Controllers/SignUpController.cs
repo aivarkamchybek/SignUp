2956f24787b9e75839d32e2ba97ff6eb9c323aef
aivarkamchybek <127583391+aivarkamchybek@users.noreply.github.com>
2024-06-30 23:57:01 -0500

    Project

---
+++ SignUp/SignUp/Controllers/SignUpController.cs
@@ -0,0 +1,99 @@
+﻿using SignUp.DAL;
+using SignUp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignUp.Controllers
+{
+    public class SignUpController : Controller
+    {
+        private SignUpDAL signUpDAL = new SignUpDAL();
+
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View(new SignUpModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(SignUpModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string profilePicturePath = null;
+
+               
+                if (model.ProfilePicture != null && model.ProfilePicture.ContentLength > 0)
+                {
+                    string fileName = Path.GetFileName(model.ProfilePicture.FileName);
+
+                   
+                    string uploadDirectory = @"C:\Users\12816\Downloads\SignUp\Uploads"; 
+                    if (!Directory.Exists(uploadDirectory))
+                    {
+                        Directory.CreateDirectory(uploadDirectory); 
+                    }
+
+                    profilePicturePath = Path.Combine(uploadDirectory, fileName);
+
+                  
+                    model.ProfilePicture.SaveAs(profilePicturePath);
+
+                    
+                    profilePicturePath = "~/Uploads/" + fileName; 
+
+                    
+                }
+
+                
+             
+                bool userAdded = signUpDAL.AddUser(model.Username, model.Email, model.Password, model.ConfirmPassword, profilePicturePath);
+
+                if (userAdded)
+                {
+                   
+                    return RedirectToAction("AllUsers");
+                }
+                else
+                {
+                    
+                    ModelState.AddModelError("", "Failed to register user. Please try again.");
+                }
+            }
+            else
+            {
+                ViewBag.ShowModal = true;
+
+            }
+
+            
+            return View(model);
+        }
+
+        public ActionResult AllUsers()
+        {
+            List<SignUpModel> users = new List<SignUpModel>();
+
+            try
+            {
+               
+                users = signUpDAL.GetAllUsers();
+            }
+            catch (Exception ex)
+            {
+               
+                ModelState.AddModelError("", "Failed to retrieve users. Please try again.");
+            }
+
+            return View(users);
+        }
+
+    }
+}
---
+++ SignUp/SignUp/DAL/SignUp.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using SignUp.Models;
+using System.Data;
+
+namespace SignUp.DAL
+{
+    public class SignUpDAL
+    {
+        private string conString = ConfigurationManager.ConnectionStrings["adoConnectionString"].ToString();
+
+        public bool AddUser(string username, string email, string password, string confirmPassword, string profilePicturePath)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(conString))
+                {
+                    string sqlQuery = "INSERT INTO UsersTbl (Username, Email, Password, ConfirmPassword, ProfilePicturePath) VALUES (@Username, @Email, @Password, @ConfirmPassword, @ProfilePicturePath)";
+                    using (SqlCommand command = new SqlCommand(sqlQuery, conn))
+                    {
+                        command.Parameters.AddWithValue("@Username", username);
+                        command.Parameters.AddWithValue("@Email", email);
+                        command.Parameters.AddWithValue("@Password", password);
+                        command.Parameters.AddWithValue("@ConfirmPassword", confirmPassword);
+                        command.Parameters.AddWithValue("@ProfilePicturePath", profilePicturePath);
+
+                        conn.Open();
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                     
+                        return rowsAffected > 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine($"Error adding user: {ex.Message}");
+                return false;
+            }
+        }
+
+        public List<SignUpModel> GetAllUsers()
+        {
+            List<SignUpModel> users = new List<SignUpModel>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(conString))
+                {
+                    string sqlQuery = "SELECT Username, Email, ProfilePicturePath FROM UsersTbl";
+                    using (SqlCommand command = new SqlCommand(sqlQuery, conn))
+                    {
+                        conn.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            SignUpModel user = new SignUpModel
+                            {
+                                Username = reader["Username"].ToString(),
+                                Email = reader["Email"].ToString(),
+                            };
+                            users.Add(user);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                throw new Exception("An error occurred while fetching users", ex);
+            }
+
+            return users;
+        }
+    }
+}
